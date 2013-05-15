7886a3e3d08eb717a72606aec1acd5c37a667326
Matti Luukkainen <mluukkai@iki.com>
2013-05-15 18:42:13 +0300

    foo

--- db/migrate/20130510175846_associate_entries_to_students.rb
+++ db/migrate/20130510175846_associate_entries_to_students.rb
@@ -2,7 +2,7 @@ class AssociateEntriesToStudents < ActiveRecord::Migration
   def up
     Entry.all.each do |e|
       student = Student.where(:student_number => e.student_number).first
-      e.student = student
+      e.student = student if e.student.nil?
       e.save
     end
   end
