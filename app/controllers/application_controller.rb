530b4be70aa4f36559e9877c2c52ca37366fdc74
Matti Luukkainen <mluukkai@iki.com>
2013-06-15 13:27:36 +0300

    öölijies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate
+  #before_filter :authenticate
 
   protected
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      if  ( username == "gndi" and password == "vallilatablehockeyleague" ) or ( username == "mluukkai" and password == "mluukkai" )
+      if  (username == "gndi" and password == "vallilatablehockeyleague") or
+          (username == "mluukkai" and password == "mluukkai")
         session[:gndi] = true
       end
-      (username == "java2100" and password == "celebration") or (username == "gndi" and password == "vallilatablehockeyleague")
+      (username == "java2100" and password == "celebration") or
+          (username == "gndi" and password == "vallilatablehockeyleague") or
+          (username == "mluukkai" and password == "mluukkai")
     end
   end
 end
